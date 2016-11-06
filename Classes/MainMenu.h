72694d9d8bc97cb436d0d87d78d6f3e523dcbfba
Mark <merksmanship@gmail.com>
2016-11-07 02:09:33 +1300

    add main menu cpp and header file

---
+++ Classes/MainMenu.h
@@ -0,0 +1,20 @@
+#ifndef __MainMenu_H__
+#define __MainMenu_H__
+
+#include "cocos2d.h"
+
+class MainMenu : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(MainMenu);
+};
+
+#endif // __MainMenu_H__
